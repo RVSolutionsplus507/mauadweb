ef5fb6ed5fba696f9e8c8588373a0b0089255b03
RVSolutions <rvs@RVSolusionssMBP.lan>
2024-09-22 22:39:06 -0500

    Colocando Home como index :rocket:

--- src/main.jsx
+++ src/main.jsx
@@ -19,6 +19,10 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       {
+        index: true,
+        element: <Home />,
+      },
+      {
         path: "/home",
         element: <Home />,
       },
